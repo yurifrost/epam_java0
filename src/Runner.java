d2be7ef37402a7cc9092f4bfe19ed6e3d7ea5833
Yuri Moroz <yuramoroz79@gmail.com>
2018-10-21 19:51:23 +0300

    Task1 comleted

--- src/Runner.java
+++ src/Runner.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 public class Runner {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter dragon's age: " + sc);
-
+        System.out.print("Enter dragon's age: ");
+        Scanner in = new Scanner(System.in);
+        int age = in.nextInt();
+        int heads = 3;
+        for (int cur_age = 0; cur_age < 200; cur_age++){
+            heads = heads + 3;
+        }
+        for (int cur_age = 200; cur_age < 300; cur_age++){
+            heads += 2;
+        }
+        for (int cur_age = 300; cur_age < age; cur_age++){
+            heads += 1;
+        }
+        System.out.println("Number of heads: " + heads);
     }
 }
